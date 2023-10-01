cc5725106776d81bf4e2301b1bba54741e01abb7
Nathaly Bataglia <nathalybataglia@outlook.com>
2023-10-01 16:49:03 -0300

    Projeto MeuPokedex

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const cors = require('cors');
+const aplicativo = express();
+aplicativo.use(bodyParser.json());
+aplicativo.use(cors());
+
+let listaUsuarios = [{ nomeUsuario: 'nath', senhaUsuario: '123' }];
+let listaPokemons = [];
+
+aplicativo.post('/acessar', (req, res) => {
+    const { nomeUsuario, senhaUsuario } = req.body;
+    if(!nomeUsuario || !senhaUsuario) return res.status(400).send({ 
+        erro: 'Preencha os campos de Login e Senha' 
+    });
+    
+    const usuario = listaUsuarios.find(u => u.nomeUsuario === nomeUsuario && u.senhaUsuario === senhaUsuario);
+    if(usuario) res.send({ 
+        sucesso: true 
+    });
+
+    else res.status(401).send({ 
+        sucesso: false, erro: 'Usuário ou Senha inválidos' 
+    });
+});
+
+aplicativo.post('/registrar-pokemon', async (req, res) => {
+    try {
+        const { idPokemon, apelidoPokemon } = req.body;
+        if(!idPokemon || !apelidoPokemon) return res.status(400).send({ 
+            erro: 'Os campos Id do Pokemon e Apelido são necessários' 
+        });
+
+        const pokemonJaExiste = listaPokemons.find(p => p.apelidoPokemon === apelidoPokemon);
+        if(pokemonJaExiste) return res.status(400).send({ 
+            erro: 'Este apelido já está sendo usado' 
+        });
+
+        const resposta = await axios.get(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`);
+        const dadosPokemon = {
+            nome: resposta.data.name,
+            imagem: resposta.data.sprites.front_default,
+            apelidoPokemon
+        };
+        
+        listaPokemons.push(dadosPokemon);
+        res.send(dadosPokemon);
+    } catch (erro) {
+        res.status(400).send({ 
+            erro: 'Erro ao registrar o Pokemon' 
+        });
+    }
+});
+
+aplicativo.get('/listar-pokemons', (req, res) => res.send(listaPokemons));
+
+aplicativo.listen(3000, () => console.log('Servidor ativo na porta 3000'));
--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [nomeUsuario, setNomeUsuario] = useState('');
+  const [senhaUsuario, setSenhaUsuario] = useState('');
+  const [usuarioLogado, setUsuarioLogado] = useState(false);
+  const [idPokemon, setIdPokemon] = useState('');
+  const [apelidoPokemon, setApelidoPokemon] = useState('');
+  const [listaPokemons, setListaPokemons] = useState([]);
+
+  const acessar = async () => {
+    try {
+      const resposta = await fetch('http://localhost:3000/acessar', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ nomeUsuario, senhaUsuario })
+      });
+      
+      const dados = await resposta.json();
+      if (dados.sucesso) setUsuarioLogado(true);
+      else alert('Login ou senha incorretos');
+    } catch (erro) {
+      console.error('Erro ao acessar', erro);
+    }
+  };
+
+  const registrarPokemon = async () => {
+    try {
+      const resposta = await fetch('http://localhost:3000/registrar-pokemon', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ idPokemon, apelidoPokemon })
+      });
+
+      if (!resposta.ok) {
+        const erro = await resposta.json();
+        console.error('Erro ao registrar Pokemon:', erro.mensagem || 'Erro desconhecido');
+        return;
+      }
+      
+      const dados = await resposta.json();
+      setListaPokemons(anterior => [...anterior, dados]);
+    } catch (erro) {
+      console.error('Erro ao registrar Pokemon', erro);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="formulario">
+        {!usuarioLogado ? (
+          <div id="formulario-acesso">
+            <h2>Bem vindo à MeuPokedex!</h2>
+            <h3>Faça seu login!</h3>
+            <label>
+              Login: <input type="text" value={nomeUsuario} onChange={(e) => setNomeUsuario(e.target.value)} />
+            </label>
+            <label>
+              Senha: <input type="password" value={senhaUsuario} onChange={(e) => setSenhaUsuario(e.target.value)} />
+            </label>
+            <button onClick={acessar}>Acessar</button>
+          </div>
+        ) : (
+          <div id="formulario-pokemon">
+            <h2>Cadastro de pokemons</h2>
+            <label>
+              Id do Pokemon: <input type="number" value={idPokemon} onChange={(e) => setIdPokemon(e.target.value)} />
+            </label>
+            <label>
+              Apelido: <input type="text" value={apelidoPokemon} onChange={(e) => setApelidoPokemon(e.target.value)} />
+            </label>
+            <button onClick={registrarPokemon}>Registrar</button>
+          </div>
+        )}
+      </div>
+
+      {listaPokemons.length > 0 && (
+        <table id="tabela-pokemons">
+          <thead>
+            <h2>Pokemons cadastrados</h2>
+            <tr>
+              <th>Imagem</th>
+              <th>Nome</th>
+              <th>Apelido</th>
+            </tr>
+          </thead>
+          <tbody id="corpo-tabela-pokemons">
+            {listaPokemons.map((pokemon, indice) => (
+              <tr key={indice}>
+                <td><img src={pokemon.imagem} alt={pokemon.nome} /></td>
+                <td>{pokemon.nome}</td>
+                <td>{pokemon.apelidoPokemon}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
